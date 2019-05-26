0349d59e053e0441d0fab0c3b4be4507b9b26612
kenttc <kenttc@gmail.com>
2019-05-26 21:17:40 +0100

    failing backwards test

--- PlutoRoverTests/UnitTest1.cs
+++ PlutoRoverTests/UnitTest1.cs
@@ -21,14 +21,27 @@ namespace PlutoRoverTests
         }
 
         [TestMethod]
-        public void given_rover_location_and_send_command_when_get_position_called_will_report_current_position()
+        public void given_rover_location_and_send_command_forward_when_get_position_called_will_report_current_position()
         {
             var currentRoverLocation = new string[] { "0", "0", "N" };
             var expectedRoverPosition = new string[] { "0", "1", "N" };
             var rover = new Rover(currentRoverLocation);
             rover.SendCommand("F");
             var afterMovingPosition = rover.GetPosition();
-            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition));
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+                , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
+        }
+
+        [TestMethod]
+        public void given_rover_location_and_send_command_back_when_get_position_called_will_report_current_position()
+        {
+            var currentRoverLocation = new string[] { "0", "1", "N" };
+            var expectedRoverPosition = new string[] { "0", "0", "N" };
+            var rover = new Rover(currentRoverLocation);
+            rover.SendCommand("B");
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+                , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
         }
     }
 }
