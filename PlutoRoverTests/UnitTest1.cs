b402c65731b1883cefc227ce93d1180dd87d2782
kenttc <kenttc@gmail.com>
2019-05-26 21:06:18 +0100

    failing simplest test

--- PlutoRoverTests/UnitTest1.cs
+++ PlutoRoverTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PlutoRoverTests
@@ -6,9 +8,29 @@ namespace PlutoRoverTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod]//and_move_command_
+        public void given_rover_location_when_get_position_called_will_report_current_position()
         {
+            var currentRoverLocation = new string[]{"0", "0", "N"};
+            var expectedRoverPosition= new string[] { "0", "0", "N" };
+            var rover = new Rover(currentRoverLocation);
+
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition));
+
+        }
+    }
+
+    public class Rover
+    {
+        public Rover(string[] currentRoverLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetPosition()
+        {
+            throw new NotImplementedException();
         }
     }
 }
