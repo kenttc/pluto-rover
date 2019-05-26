0d51eddbe18a8a02bb28e945372574f2bf767045
kenttc <kenttc@gmail.com>
2019-05-26 21:18:53 +0100

    failing turn right

--- PlutoRoverTests/UnitTest1.cs
+++ PlutoRoverTests/UnitTest1.cs
@@ -43,5 +43,16 @@ namespace PlutoRoverTests
             Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
                 , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
         }
+        [TestMethod]
+        public void given_rover_location_and_send_command_right_when_get_position_called_will_report_current_position()
+        {
+            var currentRoverLocation = new string[] { "0", "0", "N" };
+            var expectedRoverPosition = new string[] { "0", "0", "E" };
+            var rover = new Rover(currentRoverLocation);
+            rover.SendCommand("R");
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+                , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
+        }
     }
 }
