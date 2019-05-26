8b8c9c6cb580b917de1a9dd01c78cd15096b9c72
kenttc <kenttc@gmail.com>
2019-05-26 21:14:34 +0100

    failing test for movement - now uncommented.

--- PlutoRoverTests/UnitTest1.cs
+++ PlutoRoverTests/UnitTest1.cs
@@ -20,15 +20,15 @@ namespace PlutoRoverTests
                 , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
         }
 
-        //[TestMethod]
-        //public void given_rover_location_and_send_command_when_get_position_called_will_report_current_position()
-        //{
-        //    var currentRoverLocation = new string[]{"0", "0", "N"};
-        //    var expectedRoverPosition= new string[] { "0", "1", "N" };
-        //    var rover = new Rover(currentRoverLocation);
-        //    rover.SendCommand("F");
-        //    var afterMovingPosition = rover.GetPosition();
-        //    Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition));
-        //}
+        [TestMethod]
+        public void given_rover_location_and_send_command_when_get_position_called_will_report_current_position()
+        {
+            var currentRoverLocation = new string[] { "0", "0", "N" };
+            var expectedRoverPosition = new string[] { "0", "1", "N" };
+            var rover = new Rover(currentRoverLocation);
+            rover.SendCommand("F");
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition));
+        }
     }
 }
