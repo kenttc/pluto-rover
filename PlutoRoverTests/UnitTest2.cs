0f4ee25cac9954b0143a0010b02049e8a3a3d7f3
Ken Chin <kenttc@hotmail.com>
2019-06-01 16:32:18 +0100

    failing test for moving on x axis

--- PlutoRoverTests/UnitTest2.cs
+++ PlutoRoverTests/UnitTest2.cs
@@ -47,8 +47,8 @@ namespace PlutoRoverTests
         [TestMethod]
         public void given_rover_is_sent_move_f_or_b_command_will_be_able_to_move_x_plane()
         {
-            //SendMoveAndAssertLocation(new string[] {"0", "0", "E"}
-            //    , "F", new string[] {"1", "0", "E"});
+            SendMoveAndAssertLocation(new string[] { "0", "0", "E" }
+                , "F", new string[] { "1", "0", "E" });
             //SendMoveAndAssertLocation(new string[] {"0", "2", "N"}
             //    , "B", new string[] {"0", "1", "N"});
 
