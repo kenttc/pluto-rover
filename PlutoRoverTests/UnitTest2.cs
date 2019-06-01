c0c0e166b46dbb52288d0474b33b6f1d6753b9d2
Ken Chin <kenttc@hotmail.com>
2019-06-01 15:43:46 +0100

    forgot to checkin for test

---
+++ PlutoRoverTests/UnitTest2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlutoRoverTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void given_rover_default_when_get_position_called_will_report_default_position()
+        {
+             var expectedRoverPosition = new string[] { "0", "0", "N" };
+            var rover = new RoverNumber2();
+
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+                , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
+        }
+        [TestMethod]
+        public void given_rover_location_when_get_position_called_will_report_current_position()
+        {
+             var currentRoverPosition = new string[] { "1", "1", "N" };
+             var expectedRoverPosition = new string[] { "1", "1", "N" };
+            var rover = new RoverNumber2();
+            rover.SetCurrentPosition(currentRoverPosition);
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+                , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
+        }
+     
+        [TestMethod]
+        public void given_rover_is_sent_move_f_or_b_command_will_be_able_to_move()
+        {
+            SendMoveAndAssertLocation(new string[] { "0", "0", "N" }
+                , "F", new string[] { "0", "1", "N" });
+            SendMoveAndAssertLocation(new string[] { "0", "2", "N" }
+                , "B", new string[] { "0", "1", "N" });
+        }
+     
+        
+
+        public void SendMoveAndAssertLocation(string[] currentRoverLocation, string move,
+            string[] expectedRoverPosition)
+        {
+            var rover = new RoverNumber2();
+            rover.SetCurrentPosition(currentRoverLocation);
+            rover.SendCommand(move);
+            var afterMovingPosition = rover.GetPosition();
+            Assert.IsTrue(expectedRoverPosition.SequenceEqual(afterMovingPosition)
+            , $"expected {string.Join(",", expectedRoverPosition)} but got {string.Join(",", afterMovingPosition)}");
+
+
+        }
+    }
+
+    public class RoverNumber2
+    {
+        private string[] _currentRoverLocation = new string[] { "0", "0", "N" };
+
+  
+
+        public string[] GetPosition()
+        {
+            return _currentRoverLocation;
+        }
+
+        public void SetCurrentPosition(string[] currentRoverPosition)
+        {
+
+            _currentRoverLocation = currentRoverPosition;
+        }
+
+        public void SendCommand(string move)
+        {
+            if (move == "F")
+            {
+                _currentRoverLocation[1] = (Convert.ToInt32(_currentRoverLocation[1])+1).ToString();
+            }
+            else if(move == "B")
+            {
+                _currentRoverLocation[1] = (Convert.ToInt32(_currentRoverLocation[1]) - 1).ToString();
+            }
+        }
+    }
+}
